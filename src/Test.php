4e0fcc7df06a9b29b0e598a1007c5c3b0678b213
alex cai <cyy0523xc@gmail.com>
2015-08-06 17:31:31 +0800

    first commit

---
+++ src/Test.php
@@ -0,0 +1,19 @@
+<?php
+/** 
+ * 测试
+ * @author Alex <cyy0523xc@gmail.com>
+ * @copyright IBBD.net
+ * @see 
+ * @todo 
+ * @version 2015-08-06
+ */
+namespace Ibbdtest;
+
+class Test
+{
+    public function test($name = "world")
+    {
+        echo "hello {$name}!\n";
+    }
+}
+
